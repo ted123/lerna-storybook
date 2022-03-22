c70cd95d47e65f085d5c731e6fee63b5ec649f4f
Dedito Cantila Jr <dedito.cantila@gmail.com>
2022-03-22 20:15:56 +0800

    chore: add react runner

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,6 +3,6 @@ module.exports = {
     builder: "webpack5",
   },
   stories: ['../packages/**/stories/*.stories.(tsx|mdx)'],
-  addons: ['@storybook/addon-docs', '@storybook/addon-essentials'],
+  addons: ['@storybook/addon-docs', '@storybook/addon-essentials', 'storybook-addon-react-runner'],
   framework: '@storybook/react'
 };
---
+++ .storybook/preview.js
@@ -0,0 +1,8 @@
+import { Button } from '../packages/Button/src';
+
+export const parameters = {
+  reactRunner: {
+    scope: { Button },
+    language: 'tsx',
+  },
+};
